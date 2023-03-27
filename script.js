cdb7df6773d88dae574f72c72456d9096d0257a1
Jake O <56329025+jakeorch@users.noreply.github.com>
2023-03-26 20:51:16 -0600

    Update bunny and other design stuff

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function hoverBunny() {
 }
 
 function offBunny() {
-    document.getElementById('bunnyLogo').src = 'https://bunnies.jakeo.dev/images/solid-blue-gray-bunny.png';
+    document.getElementById('bunnyLogo').src = 'https://bunnies.jakeo.dev/images/solid-gradient-blue-gray-bunny.png';
 }
 
 let quotes = [
@@ -40,5 +40,37 @@ function setDesc() {
 }
 
 document.getElementById('secretComma').addEventListener('click', function (event) {
-    document.getElementById('commaFan').classList.remove('hidden');
+    document.getElementById('commaFan').classList.toggle('hidden');
 });
+
+function theme() {
+    document.querySelector('body').classList.remove('bg-gray-900');
+    document.querySelector('body').classList.add('bg-gradient-to-br');
+    document.querySelector('body').classList.add('from-indigo-400');
+    document.querySelector('body').classList.add('via-lime-500');
+    document.querySelector('body').classList.add('to-amber-500');
+    document.querySelector('body').classList.remove('text-gray-100');
+    document.querySelector('body').classList.add('text-gray-800');
+
+    document.querySelector('#fullHeader').classList.remove('from-gray-900/80');
+    document.querySelector('#fullHeader').classList.add('from-white/40');
+
+    document.querySelector('#bio').classList.remove('bg-gray-800');
+    document.querySelector('#bio').classList.add('bg-gray-200');
+
+    var elements = document.getElementsByClassName('proj');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add('bg-gray-200');
+        elements[i].classList.add('border-gray-300/40');
+    }
+
+    var elements2 = document.getElementsByClassName('pImg');
+    for (let i = 0; i < elements2.length; i++) {
+        elements2[i].classList.add('border-gray-300/70');
+    }
+
+    var elements3 = document.getElementsByClassName('title');
+    for (let i = 0; i < elements3.length; i++) {
+        elements3[i].className = 'title text-2xl font-semibold text-gray-900';
+    }
+}
