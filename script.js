19bdbd367d159eba5f854839cfda6ccfbb08de91
Jake O <56329025+jakeorch@users.noreply.github.com>
2023-07-18 06:45:59 -0700

    Add videos section and cycle through multiple images for some projects

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ let quotes = [
     'become bunny',
     'i occasionally make websites',
     'not every website needs ai',
-    /* 'stop contracting the possessive \'have\'', */
     'who needs to think when your feet just go'
 ];
 
@@ -41,73 +40,104 @@ function setDesc() {
     document.getElementById('desc').innerText = quotes[Math.floor(Math.random() * quotes.length)];
 }
 
-/* let i = 0;
-let add = false;
-let t;
+const moneyImgs = document.querySelectorAll('.moneyImg');
+let moneyCurrent = 0;
 
-document.getElementById('header').addEventListener('mouseover', startRotate, false);
-document.getElementById('header').addEventListener('mouseout', stopRotate, false);
+setInterval(function () {
+    moneyImgs.forEach((image, index) => {
+        if (index === moneyCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
 
-function startRotate(c) {
-    time = 20;
-    if (c == 'click') {
-        time = 1;
-    }
-    t = setInterval(rotate, time);
-}
+    moneyCurrent = (moneyCurrent + 1) % moneyImgs.length;
 
-function rotate() {
-    if (i == -2) {
-        add = true;
-    } else if (i == 2) {
-        add = false;
-    }
-    if (add == true) {
-        i = round(i + 0.01);
-    } else {
-        i = round(i - 0.01);
-    }
-    document.getElementById('header').style.rotate = i + 'deg';
-}
+}, 5000);
 
-function stopRotate() {
-    clearInterval(t);
-}
+const cifraImgs = document.querySelectorAll('.cifraImg');
+let cifraCurrent = 0;
 
-function round(x) { // nearest hundredths (2nd decimal) place
-    return Math.round(x * 100) / 100;
-} */
+setInterval(function () {
+    cifraImgs.forEach((image, index) => {
+        if (index === cifraCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
+
+    cifraCurrent = (cifraCurrent + 1) % cifraImgs.length;
+
+}, 5000);
+
+const plantImgs = document.querySelectorAll('.plantImg');
+let plantCurrent = 0;
+
+setInterval(function () {
+    plantImgs.forEach((image, index) => {
+        console.log(index + ': ' + image);
+
+        if (index === plantCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
+
+    plantCurrent = (plantCurrent + 1) % plantImgs.length;
+
+}, 5000);
+
+const filterImgs = document.querySelectorAll('.filterImg');
+let filterCurrent = 0;
+
+setInterval(function () {
+    filterImgs.forEach((image, index) => {
+        if (index === filterCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
+
+    filterCurrent = (filterCurrent + 1) % filterImgs.length;
+
+}, 5000);
+
+const treeImgs = document.querySelectorAll('.treeImg');
+let treeCurrent = 0;
+
+setInterval(function () {
+    treeImgs.forEach((image, index) => {
+        if (index === treeCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
+
+    treeCurrent = (treeCurrent + 1) % treeImgs.length;
+
+}, 5000);
+
+const bunnyImgs = document.querySelectorAll('.bunnyImg');
+let bunnyCurrent = 0;
+
+setInterval(function () {
+    bunnyImgs.forEach((image, index) => {
+        if (index === bunnyCurrent) {
+            image.classList.remove('opacity-0');
+        } else {
+            image.classList.add('opacity-0');
+        }
+    });
+
+    bunnyCurrent = (bunnyCurrent + 1) % bunnyImgs.length;
+
+}, 5000);
 
 function clickBunny() {
     window.open('../../', '_self');
-
-    /* document.querySelector('body').classList.remove('bg-gray-900');
-    document.querySelector('body').classList.add('bg-gradient-to-br');
-    document.querySelector('body').classList.add('from-indigo-400');
-    document.querySelector('body').classList.add('via-lime-500');
-    document.querySelector('body').classList.add('to-amber-500');
-    document.querySelector('body').classList.remove('text-gray-100');
-    document.querySelector('body').classList.add('text-gray-800');
-
-    document.querySelector('#fullHeader').classList.remove('from-gray-900/80');
-    document.querySelector('#fullHeader').classList.add('from-white/40');
-
-    document.querySelector('#bio').classList.remove('bg-gray-800');
-    document.querySelector('#bio').classList.add('bg-gray-200');
-
-    var elements = document.getElementsByClassName('proj');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add('bg-gray-200');
-        elements[i].classList.add('border-gray-300/40');
-    }
-
-    var elements2 = document.getElementsByClassName('pImg');
-    for (let i = 0; i < elements2.length; i++) {
-        elements2[i].classList.add('border-gray-300/70');
-    }
-
-    var elements3 = document.getElementsByClassName('title');
-    for (let i = 0; i < elements3.length; i++) {
-        elements3[i].className = 'title text-2xl font-semibold text-gray-900';
-    } */
 }
