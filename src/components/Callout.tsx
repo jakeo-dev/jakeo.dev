64923116231d71105d855e239bae7313a3612b3c
jakeorch <jakeorchanianatjake@gmail.com>
2025-04-27 06:52:38 -0700

    update callout color to stand out more

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -3,8 +3,12 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Callout(props: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-200/70 rounded-lg text-left px-6 py-4 my-6 text-pretty">
-      <FontAwesomeIcon icon={faStar} className="mr-2" aria-hidden />
+    <div className="bg-gradient-to-r from-sky-500/20 to-sky-500/15 rounded-lg text-left px-6 py-4 my-6 text-pretty">
+      <FontAwesomeIcon
+        icon={faStar}
+        className="text-sky-900 mr-2"
+        aria-hidden
+      />
       <p className="inline text-left">{props.children}</p>
     </div>
   );
