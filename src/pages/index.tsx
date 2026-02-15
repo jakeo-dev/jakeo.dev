46884262abba086f3ff8cc6a46c2ebb33b3d07b6
jakeorch <jakeaorchanian@gmail.com>
2026-02-15 10:34:13 -0800

    improve home page text wrapping

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,82 +68,58 @@ export default function Home() {
       </p>
 
       {/* pairckle */}
-      <div className="mt-14 mb-3 flex gap-3 md:mb-0 md:gap-6">
+      <div className="mt-14 flow-root w-full">
         <Image
           src={require("/public/images/pairckle-ss-small.png")}
           alt="A Pairckle ranking of the best types of fries"
-          className="fade-edges h-min w-7/12 rounded-xl md:w-5/12"
+          className="fade-edges float-left mr-3 h-min w-7/12 rounded-xl sm:w-1/2 md:mr-6 md:w-5/12"
         />
-        <div>
-          <h2 className="sub1title mt-0">Pairckle</h2>
-          <p className="text">
-            Rank your favorite things easily, accurately, and pairwisely with{" "}
-            <Link href="/projects/pairckle" className="internal-link">
-              Pairckle
-            </Link>
-            .
-          </p>
-          <p className="text xs:block hidden">
-            Instead of dragging and dropping and thinking about different tiers,
-            just answer the question: Curly fries or waffle fries? SpongeBob or
-            Patrick? Help or Abbey Road?
-          </p>
-          <p className="text xs:block hidden">
-            Pairckle uses pairwise comparisons for a more accurate ranking, with
-            a clear winner, loser, and everything in between.
-          </p>
-        </div>
+        <h2 className="sub1title mt-0">Pairckle</h2>
+        <p className="text md:text-balance">
+          Rank your favorite things easily, accurately, and pairwisely with{" "}
+          <Link href="/projects/pairckle" className="internal-link">
+            Pairckle
+          </Link>
+          .
+        </p>
+        <p className="text md:text-balance">
+          Instead of dragging and dropping and thinking about different tiers,
+          just answer the question: Curly fries or waffle fries? SpongeBob or
+          Squidward? Help or Abbey Road?
+        </p>
+        <p className="text md:text-balance">
+          Pairckle uses pairwise comparisons for a more accurate ranking, with a
+          clear winner, loser, and everything in between.
+        </p>
       </div>
-      <p className="text xs:hidden">
-        Instead of dragging and dropping and thinking about different tiers,
-        just answer the question: Curly fries or waffle fries? SpongeBob or
-        Patrick? Help or Abbey Road?
-      </p>
-      <p className="text xs:hidden">
-        Pairckle uses pairwise comparisons for a more accurate ranking, with a
-        clear winner, loser, and everything in between.
-      </p>
 
       {/* everydle */}
-      <div className="mt-14 flex gap-3 md:mt-10 md:gap-6">
+      <div className="mt-14 flow-root w-full md:mt-10">
         {/* smaller top margin on bigger screen because it makes more sense visually */}
-        <div>
-          <h2 className="sub1title mt-0 text-right">Everydle</h2>
-          <p className="text text-right">
-            When Dordle, Quordle, Octordle, Sedecordle, Duotrigordle, and
-            Sexaginta-quattordle aren't enough, there's{" "}
-            <Link href="/projects/everydle" className="internal-link">
-              Everydle
-            </Link>
-            .
-          </p>
-          <p className="text 2xs:block hidden text-right">
-            No longer do you need to spend five minutes solving the daily
-            Wordle; just play all two thousand of them at once in one extremely
-            long sitting, and never worry about missing a day again.
-          </p>
-          <p className="text xs:block hidden text-right">
-            How many games can you solve before getting bored and giving up?
-          </p>
-          <p className="text xs:block hidden text-right">
-            My high score is 575.
-          </p>
-        </div>
         <Image
           src={require("/public/images/everydle-ss-small.png")}
           alt="A single game of Wordle inside Everydle being solved"
-          className="fade-edges-tall h-min w-1/3 rounded-xl md:w-1/3"
+          className="fade-edges-tall float-right ml-3 h-min w-1/3 rounded-xl sm:w-[29%] md:ml-6 md:w-1/4"
         />
+        <h2 className="sub1title mt-0 text-right">Everydle</h2>
+        <p className="text text-right md:text-balance">
+          When Dordle, Quordle, Octordle, Sedecordle, Duotrigordle, and
+          Sexaginta-quattordle aren't enough, there's{" "}
+          <Link href="/projects/everydle" className="internal-link">
+            Everydle
+          </Link>
+          .
+        </p>
+        <p className="text text-right md:text-balance">
+          No longer do you need to spend five minutes solving the daily Wordle;
+          just play all two thousand of them at once in one extremely long
+          sitting, and never worry about missing a day again.
+        </p>
+        <p className="text text-right md:text-balance">
+          How many games can you solve before getting bored and giving up?
+        </p>
+        <p className="text text-right md:text-balance">My high score is 575.</p>
       </div>
-      <p className="text 2xs:hidden text-right">
-        No longer do you need to spend five minutes solving the daily Wordle;
-        just play all 2,280 of them at once in one extremely long sitting, and
-        never worry about missing a day again.
-      </p>
-      <p className="text xs:hidden text-right">
-        How many games can you solve before getting bored and giving up?
-      </p>
-      <p className="text xs:hidden text-right">My high score is 575.</p>
 
       <Link
         href="/projects"
@@ -168,49 +144,6 @@ export default function Home() {
           />
         </div>
       </Link>
-
-      {/* projects */}
-      {/* <h1 className="sub0title">Some of my projects</h1>
-
-      <p className="text">
-        When Dordle, Quordle, Octordle, Sedecordle, Duotrigordle, and
-        Sexaginta-quattordle aren't enough, there's{" "}
-        <Link href="/projects/everydle" className="internal-link">
-          Everydle
-        </Link>
-        . Play every single game of Wordle at the same time.
-      </p>
-      <p className="text">
-        Rank your favorite things easily, accurately, and pairwisely with{" "}
-        <Link href="/projects/pairckle" className="internal-link">
-          Pairckle
-        </Link>
-        . Use pairwise comparisons for a more accurate ranking, with a clear
-        winner, loser, and everything in between.
-      </p> 
-
-      {/* more */}
-      {/* <h1 className="sub0title">What else is here?</h1>
-      <p className="text">
-        You can see{" "}
-        <Link href="/projects" className="internal-link">
-          all of my projects
-        </Link>
-        ,{" "}
-        <Link href="/blog" className="internal-link">
-          my blog posts
-        </Link>
-        , or maybe you want to look at{" "}
-        <a
-          href="https://github.com/jakeo-dev/jakeo.dev"
-          target="_blank"
-          className="link"
-          rel="noopener noreferrer"
-        >
-          the source code for this website
-        </a>
-        ?
-      </p> */}
     </>
   );
 }
