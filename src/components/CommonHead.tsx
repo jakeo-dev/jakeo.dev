2ccc7ab417c1b4d26111e7f9f177e8f31dbf3055
jakeorch <jakeaorchanian@gmail.com>
2026-03-15 11:13:36 -0700

    update favicon

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -5,11 +5,6 @@ export default function CommonHead(props: { children: React.ReactNode }) {
     <Head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        href="https://bunnies.jakeo.dev/images/solid-gradient-blue-gray-bunny.png"
-        rel="shortcut icon"
-        type="image/ico"
-      />
       {props.children}
     </Head>
   );
