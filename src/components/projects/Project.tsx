07cf231c422f61407ff76e348d359089e3c97970
jakeorch <jakeaorchanian@gmail.com>
2026-08-24 20:17:48 -0700

    update project design

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -11,6 +11,7 @@ export default function Project(props: {
   type: number;
   colors?: string;
   link: string;
+  logoBig?: boolean;
 }) {
   if (props.type == 0) {
     // current project
@@ -21,35 +22,38 @@ export default function Project(props: {
           !props.colors
             ? "hover:border-sky-600 hover:shadow-sky-500/30"
             : props.colors
-        } relative mb-4 flex h-min flex-col rounded-[14px] border-2 border-stone-200 bg-stone-50 shadow-sm shadow-neutral-300/50 transition duration-300 last:mb-0 hover:-translate-y-0.5 hover:shadow-md active:translate-y-0.5 active:shadow-none md:mb-6`}
+        } group relative mb-4 flex h-min flex-col rounded-[14px] border-2 border-stone-200 bg-stone-50 shadow-sm shadow-stone-300/50 transition duration-300 last:mb-0 hover:-translate-y-0.5 hover:shadow-md active:translate-y-0.5 active:shadow-none md:mb-6`}
       >
-        <div className="relative w-full flex-grow p-5 md:p-6">
-          <h1 className="flex items-center text-left text-base font-medium md:text-lg">
-            <img
-              src={props.logo}
-              className={`${props.showLogo ? "inline" : "hidden"} mr-1.5 w-5 md:w-6`}
-              alt={`${props.name} logo`}
-            />
-            <span>{props.name}</span>
-          </h1>
-          <h2 className="text mt-2 mb-0 leading-6 text-stone-600 md:leading-7">
-            {props.desc}
-          </h2>
-          <div
-            className={`flex flex-wrap gap-2 ${
-              props.toolsList && props.toolsList.length > 0 ? "mt-2.5" : ""
-            }`}
-          >
-            {props.toolsList &&
-              props.toolsList.length > 0 &&
-              props.toolsList.map((tool, i) => (
-                <span
-                  key={i}
-                  className="inline rounded-md bg-stone-200 px-2 py-0.75 text-xs text-stone-700 md:py-0.5 md:text-sm"
-                >
-                  {tool}
-                </span>
-              ))}
+        <img
+          src={props.logo}
+          className={`${props.showLogo ? "inline" : "hidden"} ${props.logoBig ? "top-3 right-3 w-10 sm:top-1/2 sm:right-5 sm:w-26 sm:-translate-y-1/2 sm:transform" : "top-4 right-4 w-8 sm:top-1/2 sm:right-7 sm:w-22 sm:-translate-y-1/2 sm:transform"} absolute z-0 rotate-5 transition duration-300 group-hover:rotate-2 group-active:rotate-0`}
+          alt={`${props.name} logo`}
+        />
+
+        <div className="relative w-full flex-grow p-5 md:p-6 md:pr-32">
+          <div className="relative z-10">
+            <h1 className="text-left text-base font-medium md:text-lg">
+              {props.name}
+            </h1>
+            <h2 className="text mt-2 mb-0 leading-6 text-stone-600 md:leading-7">
+              {props.desc}
+            </h2>
+            <div
+              className={`flex flex-wrap gap-2 ${
+                props.toolsList && props.toolsList.length > 0 ? "mt-2.5" : ""
+              }`}
+            >
+              {props.toolsList &&
+                props.toolsList.length > 0 &&
+                props.toolsList.map((tool, i) => (
+                  <span
+                    key={i}
+                    className="inline rounded-md bg-stone-200 px-2 py-0.75 text-xs text-stone-700 md:py-0.5 md:text-sm"
+                  >
+                    {tool}
+                  </span>
+                ))}
+            </div>
           </div>
         </div>
       </Link>
@@ -60,35 +64,39 @@ export default function Project(props: {
       <a
         href={props.link}
         target="_blank"
-        className="relative mb-4 flex flex-col rounded-[14px] border-2 border-stone-200 bg-stone-50 shadow-sm transition duration-300 hover:-translate-y-0.5 hover:border-stone-400 hover:shadow-md active:translate-y-0.5 active:shadow-none md:mb-6"
+        className="group relative mb-4 flex flex-col rounded-[14px] border-2 border-stone-200 bg-stone-50 shadow-sm transition duration-300 hover:-translate-y-0.5 hover:border-stone-400 hover:shadow-md active:translate-y-0.5 active:shadow-none md:mb-6"
       >
-        <div className="relative w-full flex-grow p-5 md:p-6">
-          <h1 className="flex items-center text-left text-base font-medium md:text-lg">
-            <img
-              src={props.logo}
-              className={`${props.showLogo ? "inline" : "hidden"} mr-1.5 w-5 md:w-6`}
-              alt={`${props.name} logo`}
-            />
-            <span>{props.name}</span>
-          </h1>
-          <h2 className="text mt-2 mb-0 leading-6 text-stone-600 md:leading-7">
-            {props.desc}
-          </h2>
-          <div
-            className={`flex flex-wrap gap-2 ${
-              props.toolsList && props.toolsList.length > 0 ? "mt-2.5" : ""
-            }`}
-          >
-            {props.toolsList &&
-              props.toolsList.length > 0 &&
-              props.toolsList.map((tool, i) => (
-                <span
-                  key={i}
-                  className="inline rounded-md bg-stone-200 px-2 py-0.75 text-xs text-stone-700 md:py-0.5 md:text-sm"
-                >
-                  {tool}
-                </span>
-              ))}
+        <img
+          src={props.logo}
+          className={`${props.showLogo ? "inline" : "hidden"} ${props.logoBig ? "top-3 right-3 w-10 sm:top-1/2 sm:right-5 sm:w-26 sm:-translate-y-1/2 sm:transform" : "top-4 right-4 w-8 sm:top-1/2 sm:right-7 sm:w-22 sm:-translate-y-1/2 sm:transform"} absolute z-0 rotate-5 transition duration-300 group-hover:rotate-2 group-active:rotate-0`}
+          alt={`${props.name} logo`}
+        />
+
+        <div className="relative w-full flex-grow p-5 md:p-6 md:pr-32">
+          <div className="relative z-10">
+            {" "}
+            <h1 className="flex items-center text-left text-base font-medium md:text-lg">
+              {props.name}
+            </h1>
+            <h2 className="text mt-2 mb-0 leading-6 text-stone-600 md:leading-7">
+              {props.desc}
+            </h2>
+            <div
+              className={`flex flex-wrap gap-2 ${
+                props.toolsList && props.toolsList.length > 0 ? "mt-2.5" : ""
+              }`}
+            >
+              {props.toolsList &&
+                props.toolsList.length > 0 &&
+                props.toolsList.map((tool, i) => (
+                  <span
+                    key={i}
+                    className="inline rounded-md bg-stone-200 px-2 py-0.75 text-xs text-stone-700 md:py-0.5 md:text-sm"
+                  >
+                    {tool}
+                  </span>
+                ))}
+            </div>
           </div>
         </div>
       </a>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,4 @@
 import CommonHead from "@/components/CommonHead";
-import MasonryLayout from "@/components/MasonryLayout";
 import Project from "@/components/projects/Project";
 
 export default function Projects() {
@@ -24,37 +23,30 @@ export default function Projects() {
       <div className="section">
         <h1 className="sub0title mt-0 mb-2">Current projects</h1>
 
-      <div className="block md:flex">
-        <MasonryLayout
-          defaultCols={1}
-          smCols={1}
-          mdCols={2}
-          lgCols={2}
-          xlCols={2}
-          className="flex w-full"
-          columnClassName="bg-clip-padding px-2 md:px-3 first:pl-0 last:pr-0"
-        >
-          <Project
-            name="Everydle"
-            desc="Every game of Wordle at the same time."
-            toolsList={["Next.js", "React", "TypeScript"]}
-            logo="https://everydle.jakeo.dev/favicon.ico"
-            showLogo
-            type={0}
-            colors="hover:border-lime-600 hover:shadow-lime-500/30"
-            link="https://everydle.jakeo.dev"
-          />
+        <div className="block md:flex">
+          <div className="w-full">
+            <Project
+              name="Everydle"
+              desc="Every game of Wordle at the same time."
+              toolsList={["Next.js", "React", "TypeScript"]}
+              logo="https://everydle.jakeo.dev/favicon.ico"
+              showLogo
+              type={0}
+              colors="hover:border-lime-600 hover:shadow-lime-500/30"
+              link="https://everydle.jakeo.dev"
+            />
 
-          <Project
-            name="Pairckle"
-            desc="Rank your favorite things easily, accurately, and pairwisely."
-            toolsList={["Next.js", "React", "TypeScript"]}
-            logo="https://pairckle.jakeo.dev/pairckle-icon.png"
-            showLogo
-            type={0}
-            colors="hover:border-orange-600 hover:shadow-orange-500/30"
-            link="https://pairckle.jakeo.dev"
-          />
+            <Project
+              name="Pairckle"
+              desc="Rank your favorite things easily, accurately, and pairwisely."
+              toolsList={["Next.js", "React", "TypeScript", "Supabase"]}
+              logo="https://pairckle.jakeo.dev/pairckle-icon.png"
+              showLogo
+              logoBig
+              type={0}
+              colors="hover:border-orange-600 hover:shadow-orange-500/30"
+              link="https://pairckle.jakeo.dev"
+            />
 
             <Project
               name="Aura Farm"
@@ -62,48 +54,50 @@ export default function Projects() {
               toolsList={["Expo", "React Native", "TypeScript"]}
               logo="https://aurafarm.codebox.so/logo.svg"
               showLogo
+              logoBig
               type={0}
               colors="hover:border-red-600 hover:shadow-red-500/30"
               link="https://aurafarm.codebox.so/"
             />
 
-          <Project
-            name="MajorMap"
-            desc="An interactive flowchart for degree progress planning."
-            toolsList={["Next.js", "React", "TypeScript"]}
-            logo="https://i.imgur.com/XMdP1LU.png"
-            showLogo
-            type={0}
-            colors="hover:border-[#5261a3] hover:shadow-[#6777c2]/30"
-            link="https://majormap.vercel.app"
-          />
+            <Project
+              name="MajorMap"
+              desc="An interactive flowchart for degree progress planning."
+              toolsList={["Next.js", "React", "TypeScript"]}
+              logo="https://i.imgur.com/XMdP1LU.png"
+              showLogo
+              type={0}
+              colors="hover:border-[#5261a3] hover:shadow-[#6777c2]/30"
+              link="https://majormap.vercel.app"
+            />
 
-          <Project
-            name="WashWise"
-            desc="Discover and reduce the impact of your showers."
-            toolsList={["Expo", "React Native", "TypeScript"]}
-            logo="https://washwise.jakeo.dev/favicon.png"
-            showLogo
-            type={0}
-            colors="hover:border-blue-600 hover:shadow-blue-500/30"
-            link="https://washwise.jakeo.dev/mobile"
-          />
+            <Project
+              name="WashWise"
+              desc="Discover and reduce the impact of your showers."
+              toolsList={["Expo", "React Native", "TypeScript"]}
+              logo="https://washwise.jakeo.dev/favicon.png"
+              showLogo
+              logoBig
+              type={0}
+              colors="hover:border-blue-600 hover:shadow-blue-500/30"
+              link="https://washwise.jakeo.dev/mobile"
+            />
 
-          <Project
-            name="JakeO.dev"
-            desc="You're already here — check out my projects and blog posts."
-            /* domain bought (made w/ google sites): sep 4 2020
-            original files created (html/css/js): may 28 2021
-            first commit to github: may 21 2022
-            switched from jorch.xyz to jakeo.dev: dec 11 2022 */
-            toolsList={["Next.js", "React", "TypeScript"]}
+            <Project
+              name="JakeO.dev"
+              desc="You're already here — check out my projects and blog posts."
+              /* domain bought (made w/ google sites): sep 4 2020
+                 original files created (html/css/js): may 28 2021
+                 first commit to github: may 21 2022
+                 switched from jorch.xyz to jakeo.dev: dec 11 2022 */
+              toolsList={["Next.js", "React", "TypeScript"]}
               logo="https://jakeo.dev/logos/j-logo-blue-round.png"
-            showLogo
-            type={0}
-            link="https://jakeo.dev"
-          />
-
-        </MasonryLayout>
+              showLogo
+              logoBig
+              type={0}
+              link="https://jakeo.dev"
+            />
+          </div>
         </div>
       </div>
 
@@ -111,16 +105,8 @@ export default function Projects() {
       <div className="section">
         <h1 className="sub0title mt-0 mb-2">Past projects</h1>
 
-      <div className="block md:flex">
-        <MasonryLayout
-          defaultCols={1}
-          smCols={1}
-          mdCols={2}
-          lgCols={2}
-          xlCols={2}
-          className="flex w-full"
-          columnClassName="bg-clip-padding px-2 md:px-3 first:pl-0 last:pr-0"
-        >
+        <div className="block md:flex">
+          <div className="w-full">
             <Project
               name="Plannter"
               desc="A high school course planner, extracurricular tracker, and college application tool all in one."
@@ -132,37 +118,37 @@ export default function Projects() {
               link="https://plannter.jakeo.dev"
             />
 
-          <Project
-            name="Cifra"
-            desc="Generate vanity numbers from your own phone number to make it unforgettable."
-            toolsList={["JavaScript"]}
-            logo="https://cifra.jakeo.dev/cifra-logo.png"
-            showLogo
+            <Project
+              name="Cifra"
+              desc="Generate vanity numbers from your own phone number to make it unforgettable."
+              toolsList={["JavaScript"]}
+              logo="https://cifra.jakeo.dev/cifra-logo.png"
+              showLogo
               type={0}
-            colors="hover:border-yellow-600"
-            link="https://cifra.jakeo.dev"
-          />
+              colors="hover:border-yellow-600"
+              link="https://cifra.jakeo.dev"
+            />
 
-          <Project
-            name="B-52's font"
-            desc="A typeface based on the B-52's logo."
-            logo=""
-            type={1}
-            link="https://b52s-font.jakeo.dev/"
-          />
+            <Project
+              name="B-52's font"
+              desc="A typeface based on the B-52's logo."
+              logo=""
+              type={1}
+              link="https://b52s-font.jakeo.dev/"
+            />
 
-          <Project
-            name="Decorate a Tree"
-            desc="Decorate your own digital Christmas tree."
-            toolsList={["JavaScript"]}
-            logo="https://decorate-a-tree.jakeo.dev/candy-cane-red-left.png"
-            showLogo
-            type={1}
-            link="https://tree.jakeo.dev"
-          />
-        </MasonryLayout>
-      </div>
+            <Project
+              name="Decorate a Tree"
+              desc="Decorate your own digital Christmas tree."
+              toolsList={["JavaScript"]}
+              logo="https://decorate-a-tree.jakeo.dev/candy-cane-red-left.png"
+              showLogo
+              type={1}
+              link="https://tree.jakeo.dev"
+            />
+          </div>
         </div>
+      </div>
     </>
   );
 }
